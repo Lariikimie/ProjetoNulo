9ddbfd53d0706d186be7f8a5bc9e2157d8c1af27
Lariikimie <Larissa_kimie@hotmail.com>
2026-03-05 23:57:26 -0300

    teste commit

--- Assets/Project/Scripts/Player/FlashlightController.cs
+++ Assets/Project/Scripts/Player/FlashlightController.cs
@@ -82,7 +82,7 @@ public class FlashlightController : MonoBehaviour
         transform.localPosition = localOffset;
     }
 
-    // ================= INPUT =======================
+    // ================= INPUT =======================aaa
 
     private void HandleInput()
     {
